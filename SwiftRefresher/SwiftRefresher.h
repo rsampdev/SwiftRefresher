64b2b6d8f9409c65df2a51ce7de9b70963f599a3
rsampdev <rsampdev@gmail.com>
2016-09-20 18:07:52 -0400

    Initial Commit

---
+++ SwiftRefresher/SwiftRefresher.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRefresher.h
+//  SwiftRefresher
+//
+//  Created by Rodney Sampson on 9/20/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRefresher.
+FOUNDATION_EXPORT double SwiftRefresherVersionNumber;
+
+//! Project version string for SwiftRefresher.
+FOUNDATION_EXPORT const unsigned char SwiftRefresherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRefresher/PublicHeader.h>
+
+
